52726bf279ce5b7fb90a813d94b06fd7f6883ecb
dku-tkpark <prof@2019-seanswer.novalocal>
2019-06-26 15:52:28 +0900

    Add a subtraction method and its test code in Java files

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,5 +1,4 @@
 public class Calc {
-    public int add(int x, int y) {
-        return x+y;
-    }
+    public int add(int x, int y) { return x+y; } 
+    public int sub(int x, int y) { return x-y; }
 }

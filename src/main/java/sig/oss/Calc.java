04200cc0d11f7b71a8907149ab3c1a6d03fe26d5
dku-tkpark <40413984+dku-tkpark@users.noreply.github.com>
2019-06-27 10:43:33 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,4 +1,12 @@
 public class Calc {
-    public int add(int x, int y) { return x+y; } 
-    public int sub(int x, int y) { return x-y; }
+    public int r=0;
+    public int add(int x, int y) { // add method
+        return x+y; 
+    } 
+    public int sub(int x, int y) { // add method
+        return x-y; 
+    }
+    public void dec(int d) { // add method
+        r -= d; 
+    }
 }
